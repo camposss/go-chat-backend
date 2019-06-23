d6ba6694c8743a990be9b72fa8bac12ffff6ad4c
Christian <ccamposs23@gmail.com>
2019-06-22 19:10:46 -0700

    Basic setup with dockerfile

---
+++ go-chat-service/README.md
@@ -0,0 +1,3 @@
+# Go Chat Service
+
+- Distributed chat service example inspired by Tutorial Edge Go-Chat-Service.
--- go-chat-service/pkg/websocket/websocket.go
+++ go-chat-service/pkg/websocket/websocket.go
@@ -1,6 +1,8 @@
 package websocket
 
 import (
+	"fmt"
+	"io"
 	"log"
 	"net/http"
 
@@ -23,40 +25,40 @@ func Upgrade(w http.ResponseWriter, r *http.Request) (*websocket.Conn, error) {
 	return conn, nil
 }
 
-// func Reader(conn *websocket.Conn) {
-// 	for {
-// 		messageType, p, err := conn.ReadMessage()
-// 		if err != nil {
-// 			log.Println(err)
-// 			return
-// 		}
-// 		fmt.Println(string(p))
-// 		if err := conn.WriteMessage(messageType, p); err != nil {
-// 			log.Println(err)
-// 			return
-// 		}
-// 	}
-// }
-// func Writer(conn *websocket.Conn) {
-// 	for {
-// 		fmt.Println("SEnding")
-// 		messageType, r, err := conn.NextReader()
-// 		if err != nil {
-// 			fmt.Println(err)
-// 			return
-// 		}
-// 		w, err := conn.NextWriter(messageType)
-// 		if err != nil {
-// 			fmt.Println(err)
-// 			return
-// 		}
-// 		if _, err := io.Copy(w, r); err != nil {
-// 			fmt.Println("err")
-// 			return
-// 		}
-// 		if err := w.Close(); err != nil {
-// 			fmt.Println(err)
-// 			return
-// 		}
-// 	}
-// }
+func Reader(conn *websocket.Conn) {
+	for {
+		messageType, p, err := conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		fmt.Println(string(p))
+		if err := conn.WriteMessage(messageType, p); err != nil {
+			log.Println(err)
+			return
+		}
+	}
+}
+func Writer(conn *websocket.Conn) {
+	for {
+		fmt.Println("Sending")
+		messageType, r, err := conn.NextReader()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		w, err := conn.NextWriter(messageType)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		if _, err := io.Copy(w, r); err != nil {
+			fmt.Println("err")
+			return
+		}
+		if err := w.Close(); err != nil {
+			fmt.Println(err)
+			return
+		}
+	}
+}
