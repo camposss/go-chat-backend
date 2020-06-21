6f37956d9801e641c448354cd6df9d2ef2de2d45
Christian <ccamposs23@gmail.com>
2020-06-21 15:22:54 -0700

    added binary to gitignore

--- main.go
+++ main.go
@@ -31,7 +31,7 @@ func setupRoutes() {
 	})
 }
 func main() {
-	fmt.Println("Distributed Chat App v0.01")
+	fmt.Println("Distributed Chat App v0.01; listening on 8080")
 	setupRoutes()
 	http.ListenAndServe(":8080", nil)
 }
